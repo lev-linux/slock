4edf614e9611ea43483c7c2a3be29aee937a5041
salahdin-ahmed <salah2112004@gmail.com>
2021-03-24 17:23:52 +0200

    Apply dwm logo patch

--- config.def.h
+++ config.def.h
@@ -7,5 +7,25 @@ static const char *group = "nogroup";
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 0;
 
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 75;
+static const int logow = 12;	/* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+	/* x	y	w	h */
+	{ 0,	3,	1,	3 },
+	{ 1,	3,	2,	1 },
+	{ 0,	5,	8,	1 },
+	{ 3,	0,	1,	5 },
+	{ 5,	3,	1,	2 },
+	{ 7,	3,	1,	2 },
+	{ 8,	3,	4,	1 },
+	{ 9,	4,	1,	2 },
+	{ 11,	4,	1,	2 },
+
+};
+
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
