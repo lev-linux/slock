9d86c803aba90b13cb582221c1d2fb0090e5d1ac
salastro <salah2112004@gmail.com>
2023-05-04 16:45:48 +0300

    `failonclear = 0`

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const char *colorname[NUMCOLS] = {
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
 
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
