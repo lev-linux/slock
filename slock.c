5af308743adce3654833f1e2d0cd51c6032c9adf
salastro <salah2112004@gmail.com>
2023-05-04 16:29:20 +0300

    Media keys

--- slock.c
+++ slock.c
@@ -25,6 +25,7 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/XF86keysym.h>
+#include <X11/XF86keysym.h>
 #include <Imlib2.h>
 
 #include "arg.h"
@@ -301,6 +302,18 @@ readpw(Display *dpy, struct xrandr *rr, struct lock **locks, int nscreens,
 			    IsPrivateKeypadKey(ksym))
 				continue;
 			switch (ksym) {
+      case XF86XK_AudioPlay:
+      case XF86XK_AudioStop:
+      case XF86XK_AudioPrev:
+      case XF86XK_AudioNext:
+      case XF86XK_AudioRaiseVolume:
+      case XF86XK_AudioLowerVolume:
+      case XF86XK_AudioMute:
+      case XF86XK_AudioMicMute:
+      case XF86XK_MonBrightnessDown:
+      case XF86XK_MonBrightnessUp:
+        XSendEvent(dpy, DefaultRootWindow(dpy), True, KeyPressMask, &ev);
+        break;
 			case XK_Return:
 				passwd[len] = '\0';
 				errno = 0;
